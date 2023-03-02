f13b0c0ce843681764b11c69e3f6714737620a1b
Mauritz N <lnouwq9e@duck.com>
2023-03-02 23:42:10 +0200

    Update to open folder rather than individual file
    The folder opened folder should contain: video.mp4, subtitle.vtt

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const { convertFileSrc } = window.__TAURI__.tauri;
 const { open } = window.__TAURI__.dialog;
+const { join } = window.__TAURI__.path;
 
 window.addEventListener("DOMContentLoaded", () => {
   const videoContainer = document.querySelector("#videoContainer");
   if (videoContainer) {
-    const formEl = videoContainer.querySelector("form");
     const buttonEl = videoContainer.querySelector("button");
 
     if (buttonEl) {
@@ -13,26 +13,51 @@ window.addEventListener("DOMContentLoaded", () => {
 
         open({
           multiple: false,
-          filters: [
-            {
-              name: "Video",
-              extensions: ["mp4", "webm", "mkv"],
-            },
-          ],
+          directory: true,
         })
-          .then((selected) => {
+          .then(async (selected) => {
             if (typeof selected === "string") {
-              const assetPath = convertFileSrc(selected, "stream");
+              if (
+                !selected.endsWith("tauri-asset-bug/test_video") &&
+                !selected.endsWith("tauri-asset-bug\\test_video")
+              ) {
+                console.warn(
+                  `Selected folder is not "tauri-asset-bug/test_video" (provided test folder)! Selected folder:`,
+                  selected
+                );
+              }
+
+              const videoPath = await join(selected, "video.mp4");
+              const subtitlePath = await join(selected, "subtitle.vtt");
+
+              const videoAssetPath = convertFileSrc(videoPath, "stream");
+              const subtitleAssetPath = convertFileSrc(subtitlePath, "stream");
 
               let videoElement = document.createElement("video");
               videoElement.setAttribute("controls", "");
-              videoElement.src = assetPath;
+              videoElement.setAttribute("crossorigin", "anonymous"); // required
 
+              let sourceElement = document.createElement("source");
+              sourceElement.setAttribute("type", "video/mp4");
+              sourceElement.src = videoAssetPath;
+
+              let trackElement = document.createElement("track");
+              trackElement.setAttribute("kind", "captions");
+              trackElement.setAttribute("label", "English captions");
+              trackElement.setAttribute("srclang", "en");
+              trackElement.setAttribute("default", "");
+              trackElement.src = subtitleAssetPath;
+
+              videoElement.appendChild(sourceElement);
+              videoElement.appendChild(trackElement);
               videoContainer.appendChild(videoElement);
               buttonEl.remove();
 
-              console.log("Video path:", selected);
-              console.log("Asset path:", assetPath);
+              console.log("Folder path:", selected);
+              console.log("Video path:", videoPath);
+              console.log("Subtitle path:", subtitlePath);
+              console.log("Asset path (video):", videoAssetPath);
+              console.log("Asset path (video):", subtitleAssetPath);
             } else {
               console.warn(
                 "Selected is not a string!",
