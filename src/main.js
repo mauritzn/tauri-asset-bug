b4f45f43bec6a00c1bfb5987800958d1f0ecd04e
Mauritz N <lnouwq9e@duck.com>
2023-03-03 00:32:40 +0200

    Update to autoload video and subtitle files
    Rather than using `dialog.open`.

--- src/main.js
+++ src/main.js
@@ -1,80 +1,49 @@
 const { convertFileSrc } = window.__TAURI__.tauri;
 const { open } = window.__TAURI__.dialog;
-const { join } = window.__TAURI__.path;
+const { join, resolve, dirname } = window.__TAURI__.path;
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   const videoContainer = document.querySelector("#videoContainer");
   if (videoContainer) {
-    const buttonEl = videoContainer.querySelector("button");
-
-    if (buttonEl) {
-      buttonEl.addEventListener("click", (event) => {
-        event.preventDefault();
-
-        open({
-          multiple: false,
-          directory: true,
-        })
-          .then(async (selected) => {
-            if (typeof selected === "string") {
-              if (
-                !selected.endsWith("tauri-asset-bug/test_video") &&
-                !selected.endsWith("tauri-asset-bug\\test_video")
-              ) {
-                console.warn(
-                  `Selected folder is not "tauri-asset-bug/test_video" (provided test folder)! Selected folder:`,
-                  selected
-                );
-              }
-
-              const videoPath = await join(selected, "video.mp4");
-              const subtitlePath = await join(selected, "subtitle.vtt");
-
-              const videoAssetPath = convertFileSrc(videoPath, "stream");
-              const subtitleAssetPath = convertFileSrc(subtitlePath, "stream");
-
-              let videoElement = document.createElement("video");
-              videoElement.setAttribute("controls", "");
-              videoElement.setAttribute("crossorigin", "anonymous"); // required
-
-              let sourceElement = document.createElement("source");
-              sourceElement.setAttribute("type", "video/mp4");
-              sourceElement.src = videoAssetPath;
-
-              let trackElement = document.createElement("track");
-              trackElement.setAttribute("kind", "captions");
-              trackElement.setAttribute("label", "English captions");
-              trackElement.setAttribute("srclang", "en");
-              trackElement.setAttribute("default", "");
-              trackElement.src = subtitleAssetPath;
-
-              videoElement.appendChild(sourceElement);
-              videoElement.appendChild(trackElement);
-              videoContainer.appendChild(videoElement);
-              buttonEl.remove();
-
-              console.log("Folder path:", selected);
-              console.log("Video path:", videoPath);
-              console.log("Subtitle path:", subtitlePath);
-              console.log("Asset path (video):", videoAssetPath);
-              console.log("Asset path (video):", subtitleAssetPath);
-            } else {
-              console.warn(
-                "Selected is not a string!",
-                typeof selected,
-                selected
-              );
-            }
-          })
-          .catch((err) => {
-            console.warn("open() threw an error!", err);
-          });
-      });
-    } else {
-      console.warn(
-        `Couldn't find "button" element inside "#videoContainer" element!`
-      );
+    let currentFolder = await resolve(".");
+    if (currentFolder.endsWith("src-tauri")) {
+      currentFolder = await dirname(currentFolder);
     }
+
+    const videoPath = await join(currentFolder, "test_video", "video.mp4");
+    const subtitlePath = await join(
+      currentFolder,
+      "test_video",
+      "subtitle.vtt"
+    );
+    const videoAssetPath = convertFileSrc(videoPath, "stream");
+    const subtitleAssetPath = convertFileSrc(subtitlePath, "stream");
+
+    let videoElement = document.createElement("video");
+    videoElement.setAttribute("controls", "");
+    videoElement.setAttribute("crossorigin", "anonymous"); // required
+
+    let sourceElement = document.createElement("source");
+    sourceElement.setAttribute("type", "video/mp4");
+    sourceElement.src = videoAssetPath;
+
+    let trackElement = document.createElement("track");
+    trackElement.setAttribute("kind", "captions");
+    trackElement.setAttribute("label", "English captions");
+    trackElement.setAttribute("srclang", "en");
+    trackElement.setAttribute("default", "");
+    trackElement.src = subtitleAssetPath;
+
+    videoContainer.innerHTML = "";
+    videoElement.appendChild(sourceElement);
+    videoElement.appendChild(trackElement);
+    videoContainer.appendChild(videoElement);
+
+    console.log("Repo folder path:", currentFolder);
+    console.log("Video path:", videoPath);
+    console.log("Subtitle path:", subtitlePath);
+    console.log("Asset path (video):", videoAssetPath);
+    console.log("Asset path (video):", subtitleAssetPath);
   } else {
     console.warn(`Couldn't find "#videoContainer" element!`);
   }
